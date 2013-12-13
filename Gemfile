495fbb242d00ee8bb9816f1fdca8208e2835aae2
Jay Byoun <jay@jaybyoun.com>
2013-12-13 15:30:11 -0500

    Fix seed dates and sale amounts

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source 'https://rubygems.org'
-
+gem 'chronic'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.1'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,13 +2,17 @@ require 'csv'
 
 datafile = Rails.root + 'db/data/sales.csv'
 
+def clean_numbers(string)
+    string.gsub('$','')
+end
+
 CSV.foreach(datafile, headers: true) do |row|
   Sale.find_or_create_by(invoice_no: row['invoice_no']) do |sale|
     sale.employee = row['employee']
     sale.customer_and_account_no = row['customer_and_account_no']
     sale.product_name = row['product_name']
-    sale.sale_date = row['sale_date']
-    sale.sale_amount = row['sale_amount']
+    sale.sale_date = Chronic.parse(row['sale_date'])
+    sale.sale_amount = clean_numbers(row['sale_amount'])
     sale.units_sold = row['units_sold']
     sale.invoice_no = row['invoice_no']
     sale.invoice_frequency = row['invoice_frequency']
